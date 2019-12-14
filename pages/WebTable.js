50458120787e7a8aa736581b5d5bbe2574b8dfa2
dharitreesahoo <dharitreesahoo1@gmail.com>
2019-12-14 22:14:30 +0530

    14thDec

---
+++ pages/WebTable.js
@@ -0,0 +1,21 @@
+class addCustomer {
+	//click the delete button based on a given name. What's the best way to find this in Protractor?
+	//got this code in stack overflow
+	//https://stackoverflow.com/questions/25878496/how-to-handle-table-data-in-protractor
+	//http://www.protractortest.org/#/api?view=ElementArrayFinder.prototype.filter
+	table() {
+		var name = 'some name';
+
+		// This is like element.all(by.css(''))
+		return $$('.table.table-stripe tr').filter(function (row) {
+			// Get the second column's text.
+			return row.$$('td').get(2).getText().then(function (rowName) {
+				// Filter rows matching the name you are looking for.
+				return rowName === name;
+			});
+		}).then(function (rows) {
+			// This is an array. Find the button in the row and click on it.
+			rows[0].$('button').click();
+		});
+	}
+}
